8bd04d6fd4e9c0f21816d6aee4ef000e92c9257a
abdallahshanaah <abdallah_m_sh@yahoo.com>
2020-08-30 15:24:48 +0300

    done

---
+++ starter-code/server.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const express = require('express');
+
+const server = express();
+
+const PORT = process.env.PORT || 3000;
+
+server.get('/data', (req, res) => {
+    let jsonData = [
+        {name: 'ahmad'},
+        { name: 'james'},
+        { name: 'yousef'}
+    ]
+    res.json(jsonData)
+})
+
+server.use(express.static('./public'));
+
+
+server.listen(PORT, () => {
+    console.log(`listening to ${PORT}`);
+})
+
